a2b9f1f6709b14e6b782882d7e0051fc2f33cf5b
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-11-13 13:33:18 -0300

    otimizando testes

--- Testes/MaximizarTeste.java
+++ Testes/MaximizarTeste.java
@@ -2,42 +2,57 @@ package Testes;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Elementos.Circulo;
 import Elementos.Retangulo;
 import Elementos.Trapezio;
 import Elementos.Triangulo;
+import Visitors.VisitanteMaximizar;
 
 public class MaximizarTeste {
 
-    @Test
-    public void testeMaximizarTrapezio(){
-        Trapezio trapezio = new Trapezio(4, 6, 5);
-        trapezio.maximizar();
+    private static VisitanteMaximizar visitante;
+    private static Circulo circuloTeste;
+    private static Retangulo retanguloTeste;
+    private static Trapezio trapezioTeste;
+    private static Triangulo trianguloTeste;
 
-        assertEquals(6.0, trapezio.getBaseMaior(), 0.01);
-        assertEquals(6.0, trapezio.getBaseMenor(), 0.01);
-        assertEquals(6.0, trapezio.getAltura(), 0.01);
+    @BeforeClass
+    public static void setup() throws Exception {
+        visitante = new VisitanteMaximizar();
+        circuloTeste = new Circulo(5);
+        retanguloTeste = new Retangulo(4, 6);
+        trapezioTeste = new Trapezio(3, 6, 7);
+        trianguloTeste = new Triangulo(4, 8);
     }
 
     @Test
-    public void testeMaximizarTriangulo(){
-        Triangulo triangulo = new Triangulo(5, 8);
-        triangulo.maximizar();
-
-        assertEquals(8, triangulo.getAltura(), 0.01);
-        assertEquals(8, triangulo.getBase(), 0.01);
+    public void testeMaximizarTrapezio() {
+        visitante.visitarTrapezio(trapezioTeste);
+        assertEquals(6.0, trapezioTeste.getBaseMaior(), 0.01);
+        assertEquals(6.0, trapezioTeste.getBaseMenor(), 0.01);
+        assertEquals(6.0, trapezioTeste.getAltura(), 0.01);
     }
 
     @Test
-    public void testeMaximizarRetangulo(){
-        Retangulo retangulo = new Retangulo(7, 4);
-        retangulo.maximizar();
+    public void testeMaximizarTriangulo() {
+        visitante.visitarTriangulo(trianguloTeste);
+        assertEquals(8, trianguloTeste.getAltura(), 0.01);
+        assertEquals(8, trianguloTeste.getBase(), 0.01);
+    }
 
-        assertEquals(7, retangulo.getAltura(), 0.01);
-        assertEquals(7, retangulo.getBase(), 0.01);
+    @Test
+    public void testeMaximizarCirculo() {
+        visitante.visitarCirculo(circuloTeste);
+        assertEquals(5, circuloTeste.getRaio(), 0.01);
+    }
+
+    @Test
+    public void testeMaximizarRetangulo() {
+        visitante.visitarRetangulo(retanguloTeste);
+        assertEquals(6, retanguloTeste.getAltura(), 0.01);
+        assertEquals(6, retanguloTeste.getBase(), 0.01);
     }
 }
-
-
-
--- Testes/VisitanteCalcularAreaTeste.java
+++ Testes/VisitanteCalcularAreaTeste.java
@@ -1,46 +1,54 @@
 package Testes;
+
 import Elementos.Circulo;
 import Elementos.Retangulo;
 import Elementos.Trapezio;
 import Elementos.Triangulo;
 import Visitors.VisitanteCalcularArea;
 
-
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class VisitanteCalcularAreaTeste {
-    
+
+    private static VisitanteCalcularArea visitante;
+    private static Circulo circuloTeste;
+    private static Retangulo retanguloTeste;
+    private static Trapezio trapezioTeste;
+    private static Triangulo trianguloTeste;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        visitante = new VisitanteCalcularArea();
+        circuloTeste = new Circulo(5);
+        retanguloTeste = new Retangulo(4, 6);
+        trapezioTeste = new Trapezio(3, 5, 7);
+        trianguloTeste = new Triangulo(4, 8);
+    }
+
     @Test
     public void testVisitarCirculo() {
-        Circulo circuloTeste = new Circulo(5);
-        VisitanteCalcularArea visitante = new VisitanteCalcularArea();
         double result = visitante.visitarCirculo(circuloTeste);
         assertEquals(78.5, result, 0.01);
     }
 
     @Test
     public void testVisitarRetangulo() {
-        Retangulo retanguloTeste = new Retangulo(4, 6);
-        VisitanteCalcularArea visitante = new VisitanteCalcularArea();
         double result = visitante.visitarRetangulo(retanguloTeste);
         assertEquals(24, result, 0.01);
     }
 
     @Test
     public void testeVisitarTrapezio() {
-        Trapezio trapezioTeste = new Trapezio(3, 5, 7);
-        VisitanteCalcularArea visitante = new VisitanteCalcularArea();
         double result = visitante.visitarTrapezio(trapezioTeste);
         assertEquals(28, result, 0.01);
-        
+
     }
 
     @Test
     public void testeVisitarTriangulo() {
-        Triangulo trianguloTeste = new Triangulo(4, 8);
-        VisitanteCalcularArea visitante = new VisitanteCalcularArea();
         double result = visitante.visitarTriangulo(trianguloTeste);
         assertEquals(16, result, 0.01);
     }
